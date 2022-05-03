da1fa61c3c421e8d8e09b74db0dcba449c7d6111
Pedro Assad <pedroassad98@gmail.com>
2022-05-03 20:22:17 -0300

    Arrays Numeros

--- exercicio2.js
+++ exercicio2.js
@@ -1,1 +1,10 @@
 const numeros = [1, 2, 3, 4, 5];
+
+let i = 0;
+
+// enquanto o i for menor que o tamanho do array
+while (i < numeros.length) {
+    //executar isso
+    console.log(numeros[i]);
+    i++
+}
