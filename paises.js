ac2ea7448774b66599f8d9cd95d4d4f1d365a776
Pedro Assad <pedroassad98@gmail.com>
2022-05-03 17:53:26 -0300

    Arrays

---
+++ paises.js
@@ -0,0 +1,11 @@
+const nomeDePaises = ['Brasil', 'Argentina', 'Venezuela', 'Bolivia', 'Marrocos'];
+
+nomeDePaises.push('Turqia');
+
+nomeDePaises.pop();
+
+nomeDePaises.unshift('Holanda');
+
+nomeDePaises.shift();
+
+console.log(nomeDePaises[2]);
