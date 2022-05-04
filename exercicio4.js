855848036a27a94477fbddf7c0150c5bd75e802e
Pedro Assad <pedroassad98@gmail.com>
2022-05-03 21:42:28 -0300

    Arrays com for contando letra

---
+++ exercicio4.js
@@ -0,0 +1,11 @@
+const palavra = 'abracadabra'; // contem 5 letras a
+
+let quantidade = 0;
+
+for (let letra of palavra) {
+    if (letra === 'a') {
+        quantidade++;
+    }
+}
+
+console.log(quantidade);
